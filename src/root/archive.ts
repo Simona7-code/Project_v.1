68f5b2e8e6a649226805ca3c34d031564bd26e23
Simona7-code <61553187+Simona7-code@users.noreply.github.com>
2023-05-18 17:17:13 +0200

    2 files modified

--- src/root/archive.ts
+++ src/root/archive.ts
@@ -21,6 +21,15 @@ export class Archive {
     }
     return libriTrovati;
   }
+  
+  //effettua un controllo case insensitive
+  contieneLibro(book: Book): boolean {
+    return this.archivio.some(item => 
+      item.titolo.toLowerCase() === book.titolo.toLowerCase() 
+    && item.autore.toLowerCase() === book.autore.toLowerCase() 
+    && item.posizione.toLowerCase() === book.posizione.toLowerCase() 
+    );
+  }
 }
   /*add(tit: string, aut: string) {
     const newDoc = new Book(tit, aut); // Crea un nuovo oggetto Libro
--- src/root/inserisci/inserisci.component.ts
+++ src/root/inserisci/inserisci.component.ts
@@ -28,13 +28,48 @@ export class InserisciComponent {
   //risultatoRicerca: string = '';
 
   formInserimento() {
+
      // Logica per gestire l'invio del modulo
     let titolo = this.book.titolo
     let posizione = this.book.posizione
     let autore = this.book.autore
+    console.log(titolo,posizione,autore);
 
-     console.log(titolo,posizione,autore);
+
     // Logica per eseguire la ricerca
+    for (const key in this.book) {
+      if (Object.prototype.hasOwnProperty.call(this.book, key)) {
+        let cleanedString = this.book[key]
+          .replace(/\s{2,}/g, ' ') // Rimuove gli spazi multipli consecutivi
+          .replace(/[^a-zA-Z0-9À-ÿ\s]/g, ''); // Rimuove caratteri non alfanumerici ma lascia caratteri accentati
+    
+          this.book[key] = cleanedString.trim(); // Rimuove spazi all'inizio e alla fine della stringa
+      }
+    }
+
+    console.log(this.book);
+
+
+    this.servizio.getArch().subscribe({
+      next: archivio => {
+        this.archivio = archivio;
+
+        console.log(this.archivio)
+
+        let contiene = this.archivio.contieneLibro(this.book)
+        console.log(contiene)
+        if (!contiene){
+          
+        }
+      
+        
+        
+      },
+      error: error => {
+        // Qui puoi gestire gli errori
+        console.error('Errore durante la richiesta:', error);
+      }
+    });
     
   }
 
