8331445e8331b9fb9df90f136bd4b4efc650fb57
Simona7-code <61553187+Simona7-code@users.noreply.github.com>
2023-05-19 18:23:24 +0200

    1 file modified

--- src/root/archive.ts
+++ src/root/archive.ts
@@ -32,10 +32,26 @@ export class Archive {
     );
   }
 
+  //aggiungi libro all'archivio
   aggiungiLibro(book: Book) {
     this.archivio.push(book)
   }
   
+  //cancellare libro da archivio
+  cancellaLibro(book: Book){
+    //metodo findIndex() sull'array archivio. Prende una funzione di callback come argomento e si esegue per ogni elemento di archivio: Restituisce l'indice dell'elemento che soddisfa la condizione, oppure -1 se nessun elemento corrisponde alla condizione.
+    const index = this.archivio.findIndex(item => 
+      item.titolo === book.titolo && 
+      item.autore === book.autore &&
+      item.posizione === book.posizione && 
+      item.nominativo === book.nominativo
+    );
+    //se l'indice restituito da findIndex() è diverso da -1. Se l'indice è diverso da -1, è stato trovato un elemento corrispondente nell'array archivio e può essere rimosso.
+    if (index !== -1) {
+      //rimuovere un elemento dall'array archivio.Prende come argomenti l'indice dell'elemento da rimuovere (index) e il numero di elementi da rimuovere (in questo caso, 1).
+      this.archivio.splice(index, 1);
+    }
+  }
 }
- 
+
 
